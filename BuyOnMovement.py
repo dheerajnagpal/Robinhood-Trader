6b4795c19e99baa4b5887de800b6e92e6b359520
DNA <dna@dheerajnagpal.com>
2020-11-24 11:57:26 -0800

    Output formatting and externalized is market open function

--- BuyOnMovement.py
+++ BuyOnMovement.py
@@ -25,7 +25,7 @@ buyThresholds = [0.99, 0.98, 0.95, 0, 0]
 # For sell, only first two values are being used. Hence, last three threshold are 2 i.e. if stock price becomes double, then only sell last three items (which are 0).
 sellStockUnits = [1, 3, 0, 0, 0]
 sellStockDollars = [50, 150, 0, 0, 0]
-sellThresholds = [1.05, 1.1, 2.0, 2.0, 2.0 ]
+sellThresholds = [1.02, 1.05, 2.0, 2.0, 2.0 ]
 
 
 #Stocks that need to be traded automatically. 
@@ -34,46 +34,35 @@ stockList = {}
 
 for key in stock_list :
     stockList[key]=[True,True,True,True,True]
-    print(key,": ", stockList[key])
+    print(f'{key:5} :{stockList[key]}')
 
 login()
-
-getHoldings()
+print('\nCurrent Holdings are')
+get_holdings()
+#Newline
+print('\nBeginning Trade Sequence')
 
 while marketOpen == True :
     for stock_name,tradeStatus in stockList.items():
-#        print(stock_name)
         quote = rs.stocks.get_quotes(stock_name)
-    #    ask_price = quote.get('ask_price')
         last_trade_price = float(quote[0]['last_trade_price'])
         previous_close = float(quote[0]['previous_close'])
-        print(stock_name + ': Last Trade price is ' + str(last_trade_price) + ' and previous close was ' + str(previous_close))
-        
+        print(f'{stock_name.rjust(5)}: Last trade price is {last_trade_price:10.3f} and previous close was {previous_close:10.3f}')
         for ctr in range(len(buyThresholds)):
             if last_trade_price/previous_close < buyThresholds[ctr] and tradeStatus[ctr] == True:
-                print(f'Buy {str(buyStockUnits[ctr]):3} stocks of {stock_name}')
+                print(f'Bought {str(buyStockUnits[ctr]):3} stocks of {stock_name}\n')
                 tradeStatus[ctr] = buy_stock_units(stock_name,buyStockUnits[ctr],last_trade_price) 
             # end if
         # End For
 
         for ctr in range(len(sellThresholds)):
             if last_trade_price/previous_close > sellThresholds[ctr] and tradeStatus[ctr] == True:
-                print(f'Sell {str(sellStockUnits[ctr]):3} stocks of {stock_name}')
+                print(f'Sold {str(sellStockUnits[ctr]):3} stocks of {stock_name}\n')
                 tradeStatus[ctr] = sell_stock_units(stock_name,sellStockUnits[ctr],last_trade_price) 
             # End If
         # End For
-
-
-#Wait for 5 minutes before running the script again. 
-    sleep(300)
-    now = datetime.now(timeZone)
-    print('Current Time is : ' + str(now))
-    if now.hour < marketStart.hour or ( now.hour == marketStart.hour and now.minute < marketStart.minute ):
-        # now is after market open
-        marketOpen = False
-    elif now.hour >= marketEnd.hour :
-        marketOpen = False
-    else:
-        marketOpen = True
-    # End If Else
+    #Wait for 5 minutes before running the script again. 
+    sleep(10)
+    marketOpen = is_market_open()
+'''    # End If Else
 # End While
